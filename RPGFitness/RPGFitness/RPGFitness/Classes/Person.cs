8094a01731d0df05563c87b973393675555961fc
Christopher Mooney <n9925279@qut.edu.au>
2018-09-12 11:53:11 +1000

    Person class created

---
+++ RPGFitness/RPGFitness/RPGFitness/Classes/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGFitness.Classes
+{
+    class Person
+    {
+        public Person()
+        {
+
+        }
+    }
+}
