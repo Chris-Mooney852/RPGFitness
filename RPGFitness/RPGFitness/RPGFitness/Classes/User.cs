dc2805d45a9d2d53b21d03629df27ef1887e4d60
Christopher Mooney <n9925279@qut.edu.au>
2018-09-19 13:11:07 +1000

    User Skeleton done

--- RPGFitness/RPGFitness/RPGFitness/Classes/User.cs
+++ RPGFitness/RPGFitness/RPGFitness/Classes/User.cs
@@ -6,9 +6,29 @@ namespace RPGFitness.Classes
 {
     class User
     {
+        private uint userID;
+        private string userName;
+        private string email;
+        private uint currentWeight;
+        private uint goalWeight;
+        private uint maxDailyIntake;
+        private uint consumedCalories;
+        private uint targetDailySteps;
+        private uint currentSteps;
+
         public User()
         {
 
+        }
+
+        public uint CalculateCaloriesLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public uint CalculateCurrentSteps()
+        {
+            throw new NotImplementedException();
         }
     }
 }
