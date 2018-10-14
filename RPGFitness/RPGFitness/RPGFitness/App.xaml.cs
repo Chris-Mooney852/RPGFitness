7929743cc0b30235774878d048b9b043565b17e5
EricSleeman <41764007+EricSleeman@users.noreply.github.com>
2018-10-14 13:50:29 +1000

    UPDATING master

--- RPGFitness/RPGFitness/RPGFitness/App.xaml.cs
+++ RPGFitness/RPGFitness/RPGFitness/App.xaml.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+using RPGFitness.Data;
+using RPGFitness.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace RPGFitness
 {
     public partial class App : Application
     {
+
+        public static ItemManager Manager { get; private set; }
+
+        private ContentPage mainPage;
+
+
         public App()
         {
             InitializeComponent();
+            Manager = new ItemManager(new RestService());
 
-            MainPage = new MainPage();
+            //Make the app a navigation based app
+            mainPage = new LoginPage();
+            MainPage = new NavigationPage(mainPage);
+
+            //Assign a navigation object to the pageNavigationManager
+            PageNavigationManager.Instance.Navigation = MainPage.Navigation;
+
         }
 
         protected override void OnStart()
