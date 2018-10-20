2367e43eb44167d21ef878720955a2fbb9824524
EricSleeman <41764007+EricSleeman@users.noreply.github.com>
2018-10-20 16:57:43 +1000

    Added loading to ingredients page

--- RPGFitness/RPGFitness/RPGFitness/Views/RecipePage.xaml.cs
+++ RPGFitness/RPGFitness/RPGFitness/Views/RecipePage.xaml.cs
@@ -12,16 +12,34 @@ namespace RPGFitness.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RecipePage : ContentPage
 	{
-		public RecipePage ()
+
+        public List<Ingredient> Ingredients;
+
+        public RecipePage ()
 		{
+            
 			InitializeComponent ();
+            
+
+
 		}
 
         protected async override void OnAppearing()
         {
             base.OnAppearing();
+
+            if (Ingredients == null)
+            {
+                LoadingLabel.Text = "Loading....";
+            }
+           
+
+            Ingredients = await App.Manager.ReturnRecipeIngredients(App.Manager.mealItem);
+
             RecipeLabel.BindingContext = App.Manager.mealItem;
-            IngredientsList.ItemsSource = await App.Manager.ReturnRecipeIngredients(App.Manager.mealItem);
+            IngredientsList.ItemsSource = Ingredients;
+
+            LoadingLabel.Text = "";
 
         }
 
