f769eef61b5f1d02355a258cc30265b860dd114a
EricSleeman <41764007+EricSleeman@users.noreply.github.com>
2018-10-24 18:23:34 +1000

    Input error handling

--- RPGFitness/RPGFitness/RPGFitness/Views/UserDetailsPage.xaml.cs
+++ RPGFitness/RPGFitness/RPGFitness/Views/UserDetailsPage.xaml.cs
@@ -21,17 +21,28 @@ namespace RPGFitness.Views
 
         public async void OnSubmitClicked(object sender, EventArgs e)
         {
-            App.Manager.newUser.CurrentWeight = Convert.ToInt16(CurrentWeight.Text);
-            App.Manager.newUser.GoalWeight = Convert.ToInt16(GoalWeight.Text);
-            App.Manager.newUser.MaxDailyIntake = App.Manager.newUser.CurrentWeight * 24;
+            try
+            {
+                App.Manager.newUser.CurrentWeight = Convert.ToInt16(CurrentWeight.Text);
+                App.Manager.newUser.GoalWeight = Convert.ToInt16(GoalWeight.Text);
+                App.Manager.newUser.MaxDailyIntake = App.Manager.newUser.CurrentWeight * 24;
 
-            await App.Manager.SaveUserAsync(App.Manager.newUser);          
+                await App.Manager.SaveUserAsync(App.Manager.newUser);
+
+                navigationManager = Data.PageNavigationManager.Instance;
+                navigationManager.ShowLoginPage();
+
+            }
+            catch (FormatException)
+            {
+                ErrorLabel.Text = "Please enter a number";
+            }
+                      
             
 
            
 
-            navigationManager = Data.PageNavigationManager.Instance;
-            navigationManager.ShowLoginPage();
+          
 
         }
 
